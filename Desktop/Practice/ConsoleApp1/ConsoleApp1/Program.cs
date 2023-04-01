0a4f1855822b7e0cee6a2f84e8f6b1303710c350
ashokkumar <ashok34589@gmail.com>
2023-04-01 09:35:31 +0530

    added

---
+++ Desktop/Practice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Add(5634, 345);// 53;
+           // Console.WriteLine(res);
+            Console.WriteLine("Hello world");
+        }
+        public static void Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+    }
+}
